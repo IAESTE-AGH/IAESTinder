e7bbf970fc45e4b658df017ac44bbccf385a5ce7
Szymon-Nowaq <117742564+Szymon-Nowaq@users.noreply.github.com>
2023-06-21 21:44:35 +0200

    gradient on the bottom of the list in specific cases

--- Frontend/src/components/MultipleQuestion/MultipleQuestion.jsx
+++ Frontend/src/components/MultipleQuestion/MultipleQuestion.jsx
@@ -1,9 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useQuestions } from "../../store/questions-context";
 import classes from "./MultipleQuestion.module.css";
 
 
 const MultipleQuestion = (props) => {
+  const myElementRef = useRef(null);
+  const [activateOverlay, setactivateOverlay] = useState(false);
+  const [overlayHeight, setOverlayHeight] = useState(50);
+
   const {
     interests,
     isContinueDisabled,
@@ -60,6 +64,20 @@ const MultipleQuestion = (props) => {
     }
   };
 
+  useEffect(() => {
+    const el = myElementRef.current;
+    console.log(el.scrollHeight ,'===', el.clientHeight)
+    if(el.scrollHeight != el.clientHeight){
+      setactivateOverlay(true);
+    }
+  }, []);
+
+  const scrollHandler = () =>{
+    const el = myElementRef.current;
+    if(el.scrollTop + el.clientHeight >= el.scrollHeight - 50)
+      setOverlayHeight(el.scrollHeight - (el.scrollTop + el.clientHeight));
+  }
+
   return (
     <section className={classes.multipleQuestionContainer}>
       <header>
@@ -69,17 +87,29 @@ const MultipleQuestion = (props) => {
           masz najwięcej wspólnego!
         </p>
       </header>
-      <ul>
-        {interests.map((word, index) => (
-          <li
-            className={`${word.marked ? classes.activeWord : ""}`}
-            key={index}
-            onClick={() => wordClickHanlder(word)}
+      <div className={classes.scrollContainer}>
+        <div 
+        className={activateOverlay ? classes.activeOverlay : classes.disabledOverlay}
+        style={activateOverlay ? {height: overlayHeight} : {}}
+        //className={classes.activeOverlay}
+        //style={{height: overlayHeight}}
+        />
+          <ul
+          className={classes.list}
+          ref={myElementRef}
+          onScroll={scrollHandler}
           >
-            {word.text}
-          </li>
-        ))}
-      </ul>
+            {interests.map((word, index) => (
+              <li
+                className={`${word.marked ? classes.activeWord : ""}`}
+                key={index}
+                onClick={() => wordClickHanlder(word)}
+              >
+                {word.text}
+              </li>
+            ))}
+          </ul>
+      </div>
       <button
         onClick={changeQuestionHandler}
         className={
